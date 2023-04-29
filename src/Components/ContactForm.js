8b5a1b4b494169b82687dede7f10ed60eccf9cf7
oluwatoyinbello <teewhybells@gmail.com>
2023-04-29 21:08:25 +0100

    axios chakra-ui implemented

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,18 +1,91 @@
-import React, {useRef} from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  VStack,
+  Heading,
+  Card,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Button,
+  Textarea,
+} from "@chakra-ui/react";
+
+import axios from "axios";
 
 const ContactForm = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const submit = async (event) => {
+    event.preventDefault();
+    const response = await axios
+      .post("https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries",
+      {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+        }
+      );
+      setName('')
+      setEmail('')
+      setMessage('')
+      setSubject('')
+  };
+
   return (
-    <div>
-        <form>
-            <input type='text' placeholder='Enter your name' required />
-            <input type='email' placeholder='Enter your email' required/>
-            <input type='text' placeholder='Enter your subject' />
-            <textarea></textarea>
-            <button>Contact us</button>
+    <Box w={["full", "md"]} m="auto" p>
+      <VStack w="full" align={["center", "center"]} bg="gray">
+        <Card w="100%">
+          <VStack bg="green.600">
+            <Heading>Contact Us</Heading>
+          </VStack>
+          <VStack p="8">
+            <form onSubmit={(event) => submit(event)}>
+              <FormControl>
+                <FormLabel>Your Name</FormLabel>
+                <Input
+                  type="text"
+                  onChange={(event) => setName(event.currentTarget.value)}
+                  required
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Your Email</FormLabel>
+                <Input
+                  type="email"
+                  onChange={(event) => setEmail(event.currentTarget.value)}
+                  required
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Subject</FormLabel>
+                <Input
+                  type="text"
+                  onChange={(event) => setSubject(event.currentTarget.value)}
+                  required
+                />
+              </FormControl>
+              <FormControl>
+                <Textarea
+                  placeholder="Message"
+                  onChange={(event) => setMessage(event.currentTarget.value)}
+                />
+              </FormControl>
+              <Button mt={4} colorScheme="teal" type="submit">
+                Submit
+              </Button>
+            </form>
+          </VStack>
+        </Card>
+      </VStack>
+    </Box>
+  );
+};
 
-        </form>
-    </div>
-  )
-}
-
-export default ContactForm
+export default ContactForm;
