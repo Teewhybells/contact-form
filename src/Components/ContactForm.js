dc38d9db4cfd4a69670a88e2da21a860ad8176ff
oluwatoyinbello <teewhybells@gmail.com>
2023-05-01 16:30:39 +0100

    add toast

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -11,19 +11,43 @@ import {
   Input,
   Button,
   Textarea,
+  Text,
+  useToast
 } from "@chakra-ui/react";
 
 import axios from "axios";
 
 const ContactForm = () => {
+  const toast = useToast()
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
 
+  
+  let emailError = false
+  let nameError = false
+  let messageError = false
+
   const submit = async (event) => {
     event.preventDefault();
-    const response = await axios
+
+    if(!email || !name || !message) {
+      emailError = true
+      nameError = true
+      messageError = true
+
+      toast({
+        position: 'bottom-center',
+        isClosable: true,
+        render: () => (
+          <Box color='white' p={3} bg='red.300'>
+            Email, Name, and Message is required.
+          </Box>
+        ),
+      })
+    } else {
+      const response = await axios
       .post("https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries",
       {
         name: name,
@@ -32,10 +56,20 @@ const ContactForm = () => {
         message: message
         }
       );
+      toast({
+        position: 'bottom-center',
+        isClosable: true,
+        render: () => (
+          <Box color='white' p={3} bg='green.500'>
+            Successfully Added
+          </Box>
+        ),
+      })
       setName('')
       setEmail('')
       setMessage('')
       setSubject('')
+    }    
   };
 
   return (
@@ -47,31 +81,31 @@ const ContactForm = () => {
           </VStack>
           <VStack p="8">
             <form onSubmit={(event) => submit(event)}>
-              <FormControl>
+              <FormControl mb='4'>
                 <FormLabel>Your Name</FormLabel>
                 <Input
+                  isInvalid={{nameError}}
                   type="text"
                   onChange={(event) => setName(event.currentTarget.value)}
-                  required
                 />
+               
               </FormControl>
-              <FormControl>
+              <FormControl mb='4'>
                 <FormLabel>Your Email</FormLabel>
                 <Input
                   type="email"
                   onChange={(event) => setEmail(event.currentTarget.value)}
-                  required
                 />
               </FormControl>
-              <FormControl>
+              <FormControl mb='4'>
                 <FormLabel>Subject</FormLabel>
                 <Input
                   type="text"
                   onChange={(event) => setSubject(event.currentTarget.value)}
-                  required
                 />
               </FormControl>
-              <FormControl>
+              <FormControl mb='4'>
+              <FormLabel>Message</FormLabel>
                 <Textarea
                   placeholder="Message"
                   onChange={(event) => setMessage(event.currentTarget.value)}
